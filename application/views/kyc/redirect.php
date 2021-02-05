46f152cd45255a3af0a25e2c159af3b351d45090
jumperx222 <zhulinskyi1990@gmail.com>
2021-02-05 13:58:39 +0200

    add redirect page

---
+++ application/views/kyc/redirect.php
@@ -0,0 +1,16 @@
+<div id="content" data-id="<?php echo $verified_id;?>">Waiting KYC status...</div>
+<script>
+    function check_status(){
+        let id =$('#content').data('id');
+        $.post("<?php echo site_url('check-verified-status'); ?>", {verified_id: id}, function (response) {
+            console.log(response);
+            if (response.success == true) {
+                alert('Your record successfully saved!');
+                window.location.href = "<?php echo site_url('private8'); ?>";
+            } else {
+                setTimeout(function(){check_status();}, 10000);
+            }
+        });
+    }
+    check_status();
+</script>
